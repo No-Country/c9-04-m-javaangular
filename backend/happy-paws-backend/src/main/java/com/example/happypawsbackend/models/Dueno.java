a9c1b76fbfdd5abd5c9900d71401ec283788f20b
Bill <bill.llach1801@gmail.com>
2023-02-13 09:02:52 -0500

    create consulta and dueño models

---
+++ backend/happy-paws-backend/src/main/java/com/example/happypawsbackend/models/Dueno.java
@@ -0,0 +1,27 @@
+package com.example.happypawsbackend.models;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+import java.util.List;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+@Entity
+public class Dueno {
+    @Id
+    @GeneratedValue
+    private int id;
+    private String nombre;
+    private String correo;
+    private int celular;
+
+    @OneToMany(targetEntity = Consulta.class, cascade = CascadeType.ALL)
+    @JoinColumn(name = "cp_fk", referencedColumnName = "id")
+    private List<Consulta> consultas;
+}
