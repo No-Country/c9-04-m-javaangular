abedfc59b4aed0282116b68c782f454d8242d8e8
Bill <bill.llach1801@gmail.com>
2023-02-22 09:00:27 -0500

    add security to the app

--- backend/happy-paws-backend/src/main/java/com/example/happypawsbackend/controllers/HomeController.java
+++ backend/happy-paws-backend/src/main/java/com/example/happypawsbackend/controllers/HomeController.java
@@ -1,6 +1,7 @@
 package com.example.happypawsbackend.controllers;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -9,5 +10,10 @@ public class HomeController {
     @GetMapping("/home")
     public String Home() {
         return "Welcome to the homepage";
+    }
+
+    @PostMapping("/crear-cita")
+    public String Consulta() {
+        return "Nueva consulta creada";
     }
 }
