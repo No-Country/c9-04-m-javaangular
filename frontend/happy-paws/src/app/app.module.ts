de0db634a22dae32a10a5ae599314dc5c9937781
Bill <bill.llach1801@gmail.com>
2023-03-02 22:39:38 -0500

    add conditional login button

--- frontend/happy-paws/src/app/app.module.ts
+++ frontend/happy-paws/src/app/app.module.ts
@@ -1,6 +1,9 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
+
 import { LeafletModule } from '@asymmetrik/ngx-leaflet';
 
 import { AppRoutingModule } from './app-routing.module';
@@ -12,6 +15,10 @@ import { FooterComponent } from './footer/footer.component';
 import { CarrosuelComponent } from './carrosuel/carrosuel.component';
 import { CalendaryComponent } from './calendary/calendary.component';
 import { MapsComponent } from './maps/maps.component';
+import { AuthInterceptor } from './helpers/auth.interceptor';
+import { CookieService } from 'ngx-cookie-service';
+import { LoginComponent } from './login/login.component';
+import { LogoutComponent } from './logout/logout.component';
 
 @NgModule({
   declarations: [
@@ -22,14 +29,24 @@ import { MapsComponent } from './maps/maps.component';
     FooterComponent,
     CarrosuelComponent,
     CalendaryComponent,
-    MapsComponent
+    MapsComponent,
+    LoginComponent,
+    LogoutComponent
   ],
   imports: [
     BrowserModule,
     LeafletModule,
-    AppRoutingModule 
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule,
+    ReactiveFormsModule,
   ],
-  providers: [],
+  providers: [
+    {
+      provide: HTTP_INTERCEPTORS, useClass: AuthInterceptor, multi: true
+    },
+    CookieService,
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- frontend/happy-paws/src/app/navbar/navbar.component.ts
+++ frontend/happy-paws/src/app/navbar/navbar.component.ts
@@ -1,19 +1,35 @@
 import { ViewportScroller } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CookieService } from 'ngx-cookie-service';
+import { AppService } from '../app.service';
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.css'],
 })
-export class NavbarComponent {
+export class NavbarComponent implements OnInit {
   open = false;
+  signedIn = false;
 
-  constructor(private viewPortscroller: ViewportScroller){
-
+  constructor(
+    private viewPortscroller: ViewportScroller, 
+    private cookieService: CookieService,
+    private appService: AppService) {
+      this.appService.getIsSignedIn.subscribe(value => this.signedIn = value);
   }
 
-  scrollToElement(elementId:string): void {
+  // signIn() {
+  //   if (this.cookieService.check('bearer_token')) {
+  //     this.appService.setIsSignedIn(true);
+  //   }
+  // }
+
+  ngOnInit() {
+  }
+
+
+  scrollToElement(elementId: string): void {
     document.getElementById(elementId)!.scrollIntoView({
       behavior: 'smooth',
     });
